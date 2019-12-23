7b3999e35b7c0beef1910253b855ef0f4de9c9aa
dilanSuraj <suraj_amarasinghe@yahoo.com>
2019-12-24 00:05:42 +0530

    changes are done

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+var mongoose = require('mongoose');
+var routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 4001
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+/* 
+ Route all requests to routes.js file
+*/
+
+app.use('/', routes);
+/*
+  Backend server is listening to port 3001
+*/
+app.listen(PORT, () => {
+    console.log('Server is listening to port 4001');
+})
